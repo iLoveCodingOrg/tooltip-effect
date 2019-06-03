8b8740abf30abfde977bd7352f20fd0be2e7aead
Noor Syed <noorsyed23@gmail.com>
2019-06-03 14:46:13 -0500

    step 1

---
+++ app.js
@@ -0,0 +1,22 @@
+// Attach event handler on each button
+// On mouseenter event, show the text inside help-text element
+// on mouseleave event set the text to ''
+
+// Loop to go over all the buttons and attach the eventhandler each time.
+
+document.addEventListener('DOMContentLoaded', ()=>{
+    const helpTextList = [
+      {
+        id: 'home',
+        text: 'You are already on home'
+      },
+      {
+        id: 'about',
+        text: 'Get the info about us'
+      },
+      {
+        id: 'contact',
+        text: 'Connect with us'
+      }
+    ]
+  })
